0ce1283eaf6ef37823323c5dca4a2d9ac98cb844
amunzur <admunzur.21@gmail.com>
2021-11-03 09:03:19 -0700

    Script before I revert back to the non snakemake version

--- workflow/scripts/analysis/filter_VarScan.R
+++ workflow/scripts/analysis/filter_VarScan.R
@@ -1,27 +1,26 @@
 # This R script filters and plots VarScan2 output. 
 
 library(tidyverse)
-library(cowplot)
 library(stringr)
+library(argparse)
 
 parser <- ArgumentParser(description = "Filter the variants called by VarScan based on various parameters")
 
 # passed from Snakemake into the script
-parser$add_argument('--THRESHOLD_ExAC_ALL', metavar='VARIABLE', type='integer', help="Variants with ExAC values more than this will be filtered out, they might be germline variants.")
+parser$add_argument('--THRESHOLD_ExAC_ALL', metavar='VARIABLE', type='double', help="Variants with ExAC values more than this will be filtered out, they might be germline variants.")
 parser$add_argument('--VALUE_Func_refGene', metavar='VARIABLE', type='character', help="Whether to exclude intronic variants or not.")
-parser$add_argument('--THRESHOLD_VarFreq', metavar='VARIABLE', type='integer', help="Minimum VAF we allow. If it above, will be filtered out. Give a fraction, not percentage.")
-parser$add_argument('--THRESHOLD_Reads2', metavar='VARIABLE', type='integer', help="Minimum number of mutant reads. If a variant has less, will be filtered out.")
-parser$add_argument('--THRESHOLD_VAF_bg_ratio', metavar='VARIABLE', type='integer', help="The ratio of VAF and the bg error rate at that position must be more than this.")
+parser$add_argument('--THRESHOLD_VarFreq', metavar='VARIABLE', type='double', help="Minimum VAF we allow. If it above, will be filtered out. Give a fraction, not percentage.")
+parser$add_argument('--THRESHOLD_Reads2', metavar='VARIABLE', type='double', help="Minimum number of mutant reads. If a variant has less, will be filtered out.")
+parser$add_argument('--THRESHOLD_VAF_bg_ratio', metavar='VARIABLE', type='double', help="The ratio of VAF and the bg error rate at that position must be more than this.")
 parser$add_argument('--DIR_varscan', metavar='DIRECTORY', type='character', help="Directory where the VarScan outputs are located.")
 parser$add_argument('--PATH_bg', metavar='FILE', type='character', help="{Path to the background error rate.}")
 parser$add_argument('--PATH_bets', metavar='FILE', type='character', help="Path to the betastasis table from this cohort.")
-parser$add_argument('--PATH_BED', metavar='FILE', type='character', help="Path to the bed file for the panel.")
+parser$add_argument('--PATH_bed', metavar='FILE', type='character', help="Path to the bed file for the panel.")
 parser$add_argument('--DIR_depth_metrics', metavar='DIRECTORY', type='character', help="Dir where we keep the depths at individivual base positions.")
 parser$add_argument('--PATH_SAVE_chip_variants', metavar='FILE', type='character', help="Path to save the cleaned up chip variants to after filtering.")
 parser$add_argument('--PATH_validated_variants', metavar='FILE', type='character', help="Path to save the validated variants file with information about whether we detected them all or not.")
 
 args <- parser$parse_args()
-
 
 # DIR_figures <- "/groups/wyattgrp/users/amunzur/chip_project/figures/main_figures/new_chip_panel_CP"
 # PATH_SAVE_chip_variants <- "/groups/wyattgrp/users/amunzur/chip_project/VarScan2_results/WBC_only/new_chip_panel_CP/snv/exonic.csv"
@@ -32,7 +31,7 @@ return_anno_output <- function(PATH_annovar) {
 	df_main <- as.data.frame(read_delim(PATH_annovar, delim = "\t"))
 
 	df <- df_main %>% 
-		mutate(Sample_name = gsub("_ANNO.vcf.hg38_multianno.txt", "", basename(PATH_annovar))) %>%
+		mutate(Sample_name = gsub(".hg38_multianno.txt", "", basename(PATH_annovar))) %>%
 		select(Sample_name, Chr, Start, Ref, Alt, Func.refGene, Gene.refGene, Func.knownGene, Gene.knownGene, AAChange.refGene, ExAC_ALL, gnomAD_exome_ALL) 
 
 	return(df)
@@ -57,26 +56,31 @@ return_varscan_output <- function(PATH_varscan) {
 }
 
 # do a merge based on column to combine metadata 
-combine_anno_varscan <- function(DIR_varscan, variant_type) {
+combine_anno_varscan <- function(DIR_varscan_snv, DIR_varscan_indel, ANNOVAR_snv_output, ANNOVAR_indel_output, variant_type) {
 
-	DIR_varscan <- file.path(DIR_varscan, variant_type)
+	# determine which dir to scan based on the cariant_type given
+	if (variant_type == "snv") {
+		DIR_varscan <- DIR_varscan_snv
+		DIR_ANNOVAR <- ANNOVAR_snv_output} 
+	else {DIR_varscan <- DIR_varscan_indel
+		DIR_ANNOVAR <- ANNOVAR_indel_output}
 
-	anno_df_list <- lapply(as.list(list.files(DIR_varscan, full.names = TRUE, pattern = "\\.vcf.hg38_multianno.txt$")), return_anno_output)
-	anno_df <- do.call(rbind, anno_df_list)
+	anno_df_list <- lapply(as.list(list.files(DIR_ANNOVAR, full.names = TRUE, pattern = "\\.hg38_multianno.txt$")), return_anno_output)
+	anno_df <- as.data.frame(do.call(rbind, anno_df_list)) %>%
+				mutate(Sample_name = gsub(".hg38_multianno.txt", "", Sample_name), 
+						Start = as.character(Start)) 
 
 	varscan_df_list <- lapply(as.list(list.files(DIR_varscan, full.names = TRUE, pattern = "\\.vcf$")), return_varscan_output)
-	varscan_df <- do.call(rbind, varscan_df_list)
+	varscan_df <- do.call(rbind, varscan_df_list) %>%
+				mutate(Start = as.character(Start)) 
 
-	if (variant_type == "snv") {combined <- merge(varscan_df, anno_df, by = c("Sample_name", "Chr", "Start", "Ref", "Alt"))} else {
-		# anno_df$Start <- anno_df$Start - 1
-		if (unique(varscan_df$Start == anno_df$Start) == TRUE) {
-			anno_df <- select(anno_df, Func.refGene, Gene.refGene, AAChange.refGene, ExAC_ALL, gnomAD_exome_ALL)
-			combined <- cbind(varscan_df, anno_df)}
-	}
+	# if (variant_type == "indel"){
+	# 	varscan_df$Alt <- gsub("\\+", "", varscan_df$Alt)
+	# 	varscan_df$Alt[grep("-", varscan_df$Alt)] <- "-"}
 
-	combined$ExAC_ALL = as.numeric(gsub(".", 0, combined$ExAC_ALL))
-	combined$gnomAD_exome_ALL = as.numeric(gsub(".", 0, combined$gnomAD_exome_ALL))
-	# combined$Reads2 = as.numeric(gsub(".", 0, combined$Reads2))
+	combined <- left_join(varscan_df, anno_df, by = c("Sample_name", "Chr", "Start", "Ref", "Alt")) %>%
+					mutate(ExAC_ALL = as.numeric(gsub(".", 0, ExAC_ALL)), 
+						gnomAD_exome_ALL = as.numeric(gsub(".", 0, gnomAD_exome_ALL)))
 
 	return(combined)
 
@@ -99,12 +103,15 @@ add_bg_error_rate <- function(variants_df, PATH_bg) {
 
 	var_pos <- variants_df$Start
 	deletion_idx <- which(variants_df$variant == "deletion")
-	var_pos[deletion_idx] <- var_pos[deletion_idx] + 1 # add 1 to the position of all the deletions
-	variants_df$Start <- var_pos
+	
+	if (length(deletion_idx) > 0) {
+		var_pos[deletion_idx] <- as.numeric(var_pos[deletion_idx]) + 1 # add 1 to the position of all the deletions
+		variants_df$Start <- var_pos}
 
 	bg <- subset(bg, pos %in% variants_df$Start) # subset to the positions we have in the variants so that the df is smaller and more managable
 
 	# this merge adds an exta col with the error rate 
+	bg$pos <- as.character(bg$pos)
 	variants_df <- left_join(variants_df, bg, by = c("Start" = "pos", "error_type" = "error_type"))
 
 	# the merge above adds two extra cols we dont want, remove those 
@@ -112,8 +119,9 @@ add_bg_error_rate <- function(variants_df, PATH_bg) {
 	variants_df$ref <- NULL
 
 	# subtract 1 from the positions after merging
-	deletion_idx <- which(variants_df$variant == "deletion")
-	variants_df$Start[deletion_idx] <- variants_df$Start[deletion_idx] - 1
+	if (length(deletion_idx) > 0) {
+		deletion_idx <- which(variants_df$variant == "deletion")
+		variants_df$Start[deletion_idx] <- as.numeric(variants_df$Start[deletion_idx]) - 1}
 
 	return(variants_df)
 }
@@ -141,27 +149,20 @@ compare_with_jacks_figure <- function(PATH_to_jack, variant_df){
 # add the depth at each variant position
 add_depth <- function(DIR_depth_metrics, variant_df) {
 
-	file_list <- list.files(DIR_depth_metrics, full.names = TRUE)
-	depth_list <- list()
+	file_name_list <- list.files(DIR_depth_metrics, full.names = TRUE)
+	depth_file_list <- lapply(file_name_list, read.delim, header = FALSE) # load the related files to a list
+	depth_file <- do.call(rbind, depth_file_list) # combine all depth files into one large one
 
-	i <- 1
-	while (i <= dim(variant_df)[1]){
+	sample_names_repeated <- mapply(rep, unlist(lapply(file_name_list, basename)), unlist(lapply(depth_file_list, nrow)))
+	sample_names_repeated <- unlist(lapply(sample_names_repeated, function(x) gsub(".txt", "", x)))
+	depth_file$Sample_name <- sample_names_repeated # add the sample names so that we can do a join based on sample names with the variant df later on
+	names(depth_file) <- c("Chrom", "Position", "Depth", "Sample_name")
+	depth_file$Position <- as.character(depth_file$Position) # add the sample names so that we can do a join based on sample names with the variant df later on
 
-		f <- read.delim(grep(variant_df$Sample_name[[i]], list.files(DIR_depth_metrics, full.names = TRUE), value = TRUE), header = FALSE)
-		names(f) <- c("chr", "pos", "depth")
-		depth <- filter(f, pos == variant_df$Position[[i]], chr == variant_df$Chrom[[i]])$depth
 
-		message(i, " ", depth)
+	combined <- left_join(variant_df, depth_file, by = c("Sample_name", "Chrom", "Position"))
 
-		depth_list <- append(depth_list, depth)
-
-		i <- i + 1
-
-	} # end of while loop
-
-	variant_df$depth <- unlist(depth_list) 
-
-	return(variant_df)
+	return(combined)
 
 }
 
@@ -177,9 +178,9 @@ add_depth <- function(DIR_depth_metrics, variant_df) {
 
 # }
 
-subset_to_panel <- function(PATH_BED, variant_df) {
+subset_to_panel <- function(PATH_bed, variant_df) {
 	
-	bed <- as.data.frame(read.delim(PATH_BED, header = FALSE, sep = "\t"))
+	bed <- as.data.frame(read.delim(PATH_bed, header = FALSE, sep = "\t"))
 
 	if (ncol(bed) == 4) {names(bed) <- c("chrom", "start", "stop", "gene")} else {names(bed) <- c("chrom", "start", "stop")}
 
@@ -238,14 +239,17 @@ MAIN <- function(THRESHOLD_ExAC_ALL,
 					THRESHOLD_VarFreq, 
 					THRESHOLD_Reads2, 
 					THRESHOLD_VAF_bg_ratio, 
-					DIR_varscan, 
+					DIR_varscan_snv, 
+					DIR_varscan_indel, 
+					ANNOVAR_snv_output, 
+					ANNOVAR_indel_output,
 					PATH_bg,
 					PATH_bets,
-					PATH_BED,
+					PATH_bed,
 					DIR_depth_metrics,
 					variant_type){
 
-	combined <- combine_anno_varscan(DIR_varscan, variant_type) # add annovar annots to the varscan outputs
+	combined <- combine_anno_varscan(DIR_varscan_snv, DIR_varscan_indel, ANNOVAR_snv_output, ANNOVAR_indel_output, variant_type) # add annovar annots to the varscan outputs
 
 	# add a new col to show if we have an indel or an snv
 	if (variant_type == "indel") {combined$variant <- ifelse(unlist(str_match(combined$Alt, "\\-|\\+")[, 1]) == "-", "deletion", "insertion")} else {
@@ -273,7 +277,7 @@ MAIN <- function(THRESHOLD_ExAC_ALL,
 	dedup <- compare_with_bets(PATH_bets, combined_not_intronic)
 	dedup <- distinct(combined_not_intronic, Chrom, Position, Ref, Alt, .keep_all = TRUE) # remove duplicated variants
 	
-	dedup <- subset_to_panel(PATH_BED, dedup) # subset to panel 
+	dedup <- subset_to_panel(PATH_bed, dedup) # subset to panel 
 	dedup <- add_depth(DIR_depth_metrics, dedup) # add depth information at these positions
 	
 	return(dedup)
@@ -308,11 +312,14 @@ snv <- MAIN(THRESHOLD_ExAC_ALL = args$THRESHOLD_ExAC_ALL,
 				THRESHOLD_VarFreq = args$THRESHOLD_VarFreq, 
 				THRESHOLD_Reads2 = args$THRESHOLD_Reads2, 
 				THRESHOLD_VAF_bg_ratio = args$THRESHOLD_VAF_bg_ratio, 
-				DIR_varscan = args$DIR_varscan, 
+				DIR_varscan_snv = args$DIR_varscan_snv,
+				DIR_varscan_indel = args$DIR_varscan_indel,
+				ANNOVAR_snv_output = args$ANNOVAR_snv_output,
+				ANNOVAR_indel_output = args$ANNOVAR_indel_output,
 				PATH_bg = args$PATH_bg,
 				PATH_bets = args$PATH_bets, 
-				PATH_BED = args$PATH_BED,
-				DIR_depth_metric = args$DIR_depth_metric, 
+				PATH_bed = args$PATH_bed,
+				DIR_depth_metrics = args$DIR_depth_metrics, 
 				"snv")
 
 indel <- MAIN(THRESHOLD_ExAC_ALL = args$THRESHOLD_ExAC_ALL, 
@@ -320,14 +327,49 @@ indel <- MAIN(THRESHOLD_ExAC_ALL = args$THRESHOLD_ExAC_ALL,
 				THRESHOLD_VarFreq = args$THRESHOLD_VarFreq, 
 				THRESHOLD_Reads2 = args$THRESHOLD_Reads2, 
 				THRESHOLD_VAF_bg_ratio = args$THRESHOLD_VAF_bg_ratio, 
-				DIR_varscan = args$DIR_varscan, 
+				DIR_varscan_snv = args$DIR_varscan_snv,
+				DIR_varscan_indel = args$DIR_varscan_indel,
+				ANNOVAR_snv_output = args$ANNOVAR_snv_output,
+				ANNOVAR_indel_output = args$ANNOVAR_indel_output,
 				PATH_bg = args$PATH_bg,
 				PATH_bets = args$PATH_bets, 
-				PATH_BED = args$PATH_BED,
-				DIR_depth_metric = args$DIR_depth_metric, 
+				PATH_bed = args$PATH_bed,
+				DIR_depth_metrics = args$DIR_depth_metrics, 
 				"indel")
 
 combine_and_save(snv = snv,
 					indel = indel, 
 					PATH_validated_variants = args$PATH_validated_variants, 
 					PATH_SAVE_chip_variants = args$PATH_SAVE_chip_variants)
+
+
+# snv <- MAIN(THRESHOLD_ExAC_ALL = THRESHOLD_ExAC_ALL, 
+# 				VALUE_Func_refGene = VALUE_Func_refGene, 
+# 				THRESHOLD_VarFreq = THRESHOLD_VarFreq, 
+# 				THRESHOLD_Reads2 = THRESHOLD_Reads2, 
+# 				THRESHOLD_VAF_bg_ratio = THRESHOLD_VAF_bg_ratio, 
+# 				DIR_varscan_snv = DIR_varscan_snv,
+# 				DIR_varscan_indel = DIR_varscan_indel,
+# 				ANNOVAR_snv_output = ANNOVAR_snv_output,
+# 				ANNOVAR_indel_output = ANNOVAR_indel_output,
+# 				PATH_bg = PATH_bg,
+# 				PATH_bets = PATH_bets, 
+# 				PATH_bed = PATH_bed,
+# 				DIR_depth_metrics = DIR_depth_metrics, 
+# 				"snv")
+
+# indel <- MAIN(THRESHOLD_ExAC_ALL = THRESHOLD_ExAC_ALL, 
+# 				VALUE_Func_refGene = VALUE_Func_refGene, 
+# 				THRESHOLD_VarFreq = THRESHOLD_VarFreq, 
+# 				THRESHOLD_Reads2 = THRESHOLD_Reads2, 
+# 				THRESHOLD_VAF_bg_ratio = THRESHOLD_VAF_bg_ratio, 
+# 				DIR_varscan_snv = DIR_varscan_snv,
+# 				DIR_varscan_indel = DIR_varscan_indel,
+# 				ANNOVAR_snv_output = ANNOVAR_snv_output,
+# 				ANNOVAR_indel_output = ANNOVAR_indel_output,
+# 				PATH_bg = PATH_bg,
+# 				PATH_bets = PATH_bets, 
+# 				PATH_bed = PATH_bed,
+# 				DIR_depth_metrics = DIR_depth_metrics, 
+# 				"indel")
+
