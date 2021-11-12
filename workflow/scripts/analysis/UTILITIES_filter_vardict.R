02d8174235e5a4cd20725db725b5cbd9c2cc3e50
amunzur <admunzur.21@gmail.com>
2021-11-12 12:19:41 -0800

    Removed the shared functions between vardict and varscan into a common shared utilities functions file

--- workflow/scripts/analysis/UTILITIES_filter_vardict.R
+++ workflow/scripts/analysis/UTILITIES_filter_vardict.R
@@ -1,16 +1,3 @@
-return_anno_output <- function(DIR_ANNOVAR) {
-
-	df_main <- as.data.frame(read_delim(DIR_ANNOVAR, delim = "\t"))
-
-	df <- df_main %>% 
-		mutate(Sample_name = gsub(".hg38_multianno.txt", "", basename(DIR_ANNOVAR))) %>%
-		select(Sample_name, Chr, Start, Ref, Alt, Func.refGene, Gene.refGene, Func.knownGene, Gene.knownGene, AAChange.refGene, ExAC_ALL, gnomAD_exome_ALL) 
-
-	return(df)
-
-}
-
-# filter anno output from all samples based on exac score and combine into one big df
 return_vardict_output <- function(DIR_vardict) {
 
 	df_main <- as.data.frame(read_delim(DIR_vardict, delim = "\t")) 
@@ -21,7 +8,7 @@ return_vardict_output <- function(DIR_vardict) {
 }
 
 # do a merge based on column to combine metadata 
-combine_anno_varscan <- function(DIR_vardict, DIR_ANNOVAR) {
+combine_anno_vardict <- function(DIR_vardict, DIR_ANNOVAR) {
 
 	# determine which dir to scan based on the cariant_type given
 
@@ -95,117 +82,6 @@ add_bg_error_rate <- function(variants_df, bg) {
 	return(variants_df)
 }
 
-# compare with the bets list, add an extra col to show if the vars we called also appear in the bets
-compare_with_bets <- function(PATH_bets, variant_df){
-
-	bets <- as.data.frame(read_delim(PATH_bets, delim = "\t"))
-	variant_df$detected <- variant_df$Position %in% bets$POSITION
-
-	# add a new col to indicate if the gene of interest in each row is found in the bets. 
-	variant_df$Gene_in_bets <- variant_df$Gene %in% bets$GENE
-
-	return(variant_df)
-}
-
-# this function compares variants to variants jack found. need a path to jack
-compare_with_jacks_figure <- function(PATH_validated_variants, variant_df){
-
-	validated_vars <- read.delim(PATH_validated_variants, header = FALSE, sep = "\t")
-	names(validated_vars) <- c("Sample", "Gene", "Chrom", "Position", "Ref", "Alt")
-
-	validated_vars$detected <- validated_vars$Position %in% variant_df$Position
-
-	return(validated_vars)
-}
-
-# add the depth at each variant position
-add_depth <- function(DIR_depth_metrics, variant_df) {
-
-	file_name_list <- list.files(DIR_depth_metrics, full.names = TRUE)
-	depth_file_list <- lapply(file_name_list, read.delim, header = FALSE) # load the related files to a list
-	depth_file <- do.call(rbind, depth_file_list) # combine all depth files into one large one
-
-	sample_names_repeated <- mapply(rep, unlist(lapply(file_name_list, basename)), unlist(lapply(depth_file_list, nrow)))
-	sample_names_repeated <- unlist(lapply(sample_names_repeated, function(x) gsub(".txt", "", x)))
-	depth_file$Sample_name <- sample_names_repeated # add the sample names so that we can do a join based on sample names with the variant df later on
-	names(depth_file) <- c("Chrom", "Position", "Depth", "Sample_name")
-	depth_file$Position <- as.character(depth_file$Position) # add the sample names so that we can do a join based on sample names with the variant df later on
-
-
-	combined <- left_join(variant_df, depth_file, by = c("Sample_name", "Chrom", "Position"))
-
-	return(combined)
-
-}
-
-# add_mutation_type <- function(variant_df){
-
-# 	Mutation_type <- as.list(str_match(unlist(variant_df$AAchange), "p.[A-z][0-9][0-9[a-zA-Z0-9]][0-9[a-zA-Z0-9]][a-zA-Z0-9]*"))
-
-# lapply(Mutation_type, substr, 3, 3)
-
-
-
-
-
-# }
-
-subset_to_panel <- function(PATH_bed, variant_df) {
-	
-	bed <- as.data.frame(read.delim(PATH_bed, header = FALSE, sep = "\t"))
-
-	if (ncol(bed) == 4) {names(bed) <- c("chrom", "start", "stop", "gene")} else {names(bed) <- c("chrom", "start", "stop")}
-
-	to_keep <- list() # list of positions to remove
-
-	print("Subsetting to the panel.")
-	i <- 1 
-	print(i)
-	while (i <= dim(variant_df)[1]){
-		chrom_subsetted <- variant_df[i, 3] # pick the chrom we are at 
-		location <- variant_df[i, 4] # pick the location we are at 
-		bed_subsetted <- bed %>% filter(chrom == chrom_subsetted) # subset the bed by chrom
-		message(c("position:", i))
-
-		j <- 1
-		while(j <= dim(bed_subsetted)[1]) {
-			start_pos <- bed_subsetted[j, 2]
-			end_pos <- bed_subsetted[j, 3]
-
-			if (all(location >= start_pos, location <= end_pos)) {
-				to_keep <- append(to_keep, i) # saving the row index of muts we are keeping
-				break 
-				print("Position in panel.")
-				} else {j <- j + 1} # if the location isn't in the panel, go check out the next position in the bed file.
-				# print(c(j, "j"))
-
-		} # end of inner while loop
-
-	i <- i + 1 # next identified variant
-
-	} # end of outer while loop - looping through identified variants
-
-	variant_df <- variant_df[unlist(to_keep), ]
-
-	return(variant_df)
-
-} # end of function
-
-# This function generates a string of explanations to add to the final df as comments. 
-add_documentation <- function(THRESHOLD_ExAC_ALL, VALUE_Func_refGene, THRESHOLD_VarFreq, THRESHOLD_Reads2, THRESHOLD_VAF_bg_ratio){
-
-	docs <- paste(paste0("Exac score: less than or equal to ", THRESHOLD_ExAC_ALL),
-				paste0("Function: not ", VALUE_Func_refGene),
-				paste0("VAF: less than or equal to:", THRESHOLD_VarFreq),
-				paste0("Number of mutant reads:", THRESHOLD_Reads2), 
-				paste0("VAF_bg_ratio", THRESHOLD_VAF_bg_ratio), 
-				sep = "\n")
-
-	return(docs)
-
-}
-
-
 # main function to run everything
 MAIN <- function(THRESHOLD_ExAC_ALL, 
 					VALUE_Func_refGene, 
@@ -220,7 +96,7 @@ MAIN <- function(THRESHOLD_ExAC_ALL,
 					DIR_depth_metrics,
 					DIR_finland_bams){
 
-	combined <- combine_anno_varscan(DIR_vardict, DIR_ANNOVAR) # add annovar annots to the varscan outputs
+	combined <- combine_anno_vardict(DIR_vardict, DIR_ANNOVAR) # add annovar annots to the varscan outputs
 
 	# add patient id
 	x <- str_split(combined$Sample_name, "-") # split the sample name
--- workflow/scripts/analysis/UTILITIES_filter_varscan.R
+++ workflow/scripts/analysis/UTILITIES_filter_varscan.R
@@ -1,24 +1,8 @@
-return_anno_output <- function(PATH_annovar) {
-
-	df_main <- as.data.frame(read_delim(PATH_annovar, delim = "\t"))
-
-	df <- df_main %>% 
-		mutate(Sample_name = gsub(".hg38_multianno.txt", "", basename(PATH_annovar))) %>%
-		select(Sample_name, Chr, Start, Ref, Alt, Func.refGene, Gene.refGene, Func.knownGene, Gene.knownGene, AAChange.refGene, ExAC_ALL, gnomAD_exome_ALL) 
-
-	return(df)
-
-}
-
-# filter anno output from all samples based on exac score and combine into one big df
 return_varscan_output <- function(PATH_varscan) {
 
 	print(PATH_varscan)
 
 	df_main <- as.data.frame(read_delim(PATH_varscan, delim = "\t")) 
-	# names(df_main) <- c("Chr", "Start", "Ref", "Cons", "Reads1", "Reads2", "VarFreq", "Strands1", "Strands2", "Qual1", 
-						# "Qual2", "Pvalue", "MapQual1", "MapQual2", "Reads1Plus", "Reads1Minus", "Reads2Plus", "Reads2Minus", "Alt", "Sample_name")
-
 	df <- df_main %>%
 			mutate(Sample_name = gsub(".vcf", "", basename(PATH_varscan))) %>%
 			rename(Chr = Chrom, Start = Position, Alt = VarAllele)
@@ -95,107 +79,6 @@ add_bg_error_rate <- function(variants_df, bg) {
 		variants_df$Start[deletion_idx] <- as.numeric(variants_df$Start[deletion_idx]) - 1}
 
 	return(variants_df)
-}
-
-# compare with the bets list, add an extra col to show if the vars we called also appear in the bets
-compare_with_bets <- function(PATH_bets, variant_df){
-
-	bets <- as.data.frame(read_delim(PATH_bets, delim = "\t"))
-	variant_df$detected <- variant_df$Position %in% bets$POSITION
-
-	# add a new col to indicate if the gene of interest in each row is found in the bets. 
-	variant_df$Gene_in_bets <- variant_df$Gene %in% bets$GENE
-
-	return(variant_df)
-}
-
-# this function compares variants to variants jack found. need a path to jack
-compare_with_jacks_figure <- function(PATH_validated_variants, variant_df){
-
-	validated_vars <- read.delim(PATH_validated_variants, header = FALSE)
-	names(validated_vars) <- c("Sample", "Gene", "Chrom", "Position", "Ref", "Alt")
-
-	validated_vars$detected <- validated_vars$Position %in% variant_df$Position
-
-	return(validated_vars)
-}
-
-# add the depth at each variant position
-add_depth <- function(DIR_depth_metrics, variant_df) {
-
-	file_name_list <- list.files(DIR_depth_metrics, full.names = TRUE)
-	depth_file_list <- lapply(file_name_list, read.delim, header = FALSE) # load the related files to a list
-	depth_file <- do.call(rbind, depth_file_list) # combine all depth files into one large one
-
-	sample_names_repeated <- mapply(rep, unlist(lapply(file_name_list, basename)), unlist(lapply(depth_file_list, nrow)))
-	sample_names_repeated <- unlist(lapply(sample_names_repeated, function(x) gsub(".txt", "", x)))
-	depth_file$Sample_name <- sample_names_repeated # add the sample names so that we can do a join based on sample names with the variant df later on
-	names(depth_file) <- c("Chrom", "Position", "Depth", "Sample_name")
-	depth_file$Position <- as.character(depth_file$Position) # add the sample names so that we can do a join based on sample names with the variant df later on
-
-	combined <- left_join(variant_df, depth_file, by = c("Sample_name", "Chrom", "Position"))
-	# x <- left_join(dedup, depth_file, by = c("Sample_name", "Chrom", "Position"))
-
-	filter(depth_file, Sample_name == "GUBB-17-094-gDNA-Baseline-IDT-2017May25_S3", Chrom == "chr2", Position == "25246696")
-
-	return(combined)
-
-}
-
-subset_to_panel <- function(PATH_bed, variant_df) {
-	
-	bed <- as.data.frame(read.delim(PATH_bed, header = FALSE, sep = "\t"))
-
-	if (ncol(bed) == 4) {names(bed) <- c("chrom", "start", "stop", "gene")} else {names(bed) <- c("chrom", "start", "stop")}
-
-	to_keep <- list() # list of positions to remove
-
-	print("Subsetting to the panel.")
-	i <- 1 
-	print(i)
-	while (i <= dim(variant_df)[1]){
-		chrom_subsetted <- variant_df[i, 3] # pick the chrom we are at 
-		location <- variant_df[i, 4] # pick the location we are at 
-		bed_subsetted <- bed %>% filter(chrom == chrom_subsetted) # subset the bed by chrom
-		message(c("position:", i))
-
-		j <- 1
-		while(j <= dim(bed_subsetted)[1]) {
-			start_pos <- bed_subsetted[j, 2]
-			end_pos <- bed_subsetted[j, 3]
-
-			if (all(location >= start_pos, location <= end_pos)) {
-				print(j)
-				print("Position in panel.")
-				to_keep <- append(to_keep, i) # saving the row index of muts we are keeping
-				break 
-				} else {j <- j + 1} # if the location isn't in the panel, go check out the next position in the bed file.
-				# print(c(j, "j"))
-
-		} # end of inner while loop
-
-	i <- i + 1 # next identified variant
-
-	} # end of outer while loop - looping through identified variants
-
-	variant_df <- variant_df[unlist(to_keep), ]
-
-	return(variant_df)
-
-} # end of function
-
-# This function generates a string of explanations to add to the final df as comments. 
-add_documentation <- function(THRESHOLD_ExAC_ALL, VALUE_Func_refGene, THRESHOLD_VarFreq, THRESHOLD_Reads2, THRESHOLD_VAF_bg_ratio){
-
-	docs <- paste(paste0("Exac score: less than or equal to ", THRESHOLD_ExAC_ALL),
-				paste0("Function: not ", VALUE_Func_refGene),
-				paste0("VAF: less than or equal to:", THRESHOLD_VarFreq),
-				paste0("Number of mutant reads:", THRESHOLD_Reads2), 
-				paste0("VAF_bg_ratio", THRESHOLD_VAF_bg_ratio), 
-				sep = "\n")
-
-	return(docs)
-
 }
 
 # main function to run everything
