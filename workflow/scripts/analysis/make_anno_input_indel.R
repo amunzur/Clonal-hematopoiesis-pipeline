754fcd5b5081b6c51a8cbbc6771345f493328565
amunzur <admunzur.21@gmail.com>
2022-05-12 14:35:03 -0700

    fixed typo

--- workflow/scripts/analysis/make_anno_input_indel.R
+++ workflow/scripts/analysis/make_anno_input_indel.R
@@ -17,70 +17,23 @@ args <- parser$parse_args()
 # works on only one path. parallelize using lapply.
 make_anno_input <- function(PATH_VarScan_indel, ANNOVAR_indel_input) {
 
-# come back to this later to make this piece of code more readable
-	df_main <- varscan_df %>%
+	df_main <- as.data.frame(read.delim(PATH_VarScan_indel)) %>%
 		mutate(
 			Var_type = case_when(
-				grepl("-", Alt) ~ "DEL", 
-				grepl("+", Alt) ~ "INS",
+				grepl("-", VarAllele) ~ "DEL", 
+				grepl("+", VarAllele) ~ "INS",
 				TRUE ~ "FUCK"), 
-			Alt = gsub("\\+|-", "", Alt), # remove - and + signs
-			Alt = ifelse(Var_type == "DEL", "-", Alt), # if DEL, alt allele must be -. Otherwise (insertion) alt column doesnt change
+			VarAllele = gsub("\\+|-", "", VarAllele), # remove - and + signs
+			VarAllele = ifelse(Var_type == "DEL", "-", VarAllele), # if DEL, alt allele must be -. Otherwise (insertion) alt column doesnt change
 			Ref = ifelse(Var_type == "INS", "-", Ref), # if INS the ref becomes -. Otherwise (DEL) it stays the same.
-			Start = ifelse(Var_type == "DEL", as.numeric(Start) + 1, as.numeric(Start)), # if DEL, add 1 to pos
-			End = ifelse(Var_type == "DEL", as.numeric(Start) + nchar(Alt) - 1, as.numeric(Start))) %>% # if DEL we add 1 to the start  
-		rename(Chrom = Chr) %>%
+			Start = Position, 
+			Start = ifelse(Var_type == "DEL", as.numeric(Position) + 1, as.numeric(Position)), # if DEL, add 1 to pos
+			End = ifelse(Var_type == "DEL", as.numeric(Start) + nchar(VarAllele) - 1, as.numeric(Start))) %>% # if DEL we add 1 to the start  
+		rename(Alt = VarAllele) %>%
 		select(Chrom, Start, End, Ref, Alt)
 
-	
-	df_main <- as.data.frame(read.delim(PATH_VarScan_indel)) %>%
-			mutate(
-				Var_type = case_when(
-					grepl("\\-", VarAllele) ~ "DEL", 
-					grepl("\\+", VarAllele) ~ "INS",
-					TRUE ~ "FUCK"), 
-				VarAllele = gsub("\\+|-", "", VarAllele))
-
-	# DELETIONS
-	df_del <- df_main %>%
-		filter(Var_type == "DEL") %>%
-		mutate(End = Position) %>%
-		select(Chrom, Position, End, VarAllele, Var_type, VarFreq) %>%
-		mutate(
-			Position = Position + 1, 
-			End = as.numeric(Position) + nchar(VarAllele) - 1, 
-			Var = "-", 
-			VarFreq = readr::parse_number(as.character(VarFreq))) %>%
-		arrange(Position, -VarFreq) %>%
-  		filter(duplicated(Position) == FALSE) %>% # If position is duplicated, only keep the variant with the higher allele frequency
-		rename(
-			Start = Position,
-			Ref = VarAllele) %>%
-		select(Chrom, Start, End, Ref, Var)
-
-	# INSERTIONS
-	df_ins <- df_main %>%
-		filter(Var_type == "INS") %>%
-		mutate(
-			End = Position, 
-			Ref = "-", 
-			VarFreq = readr::parse_number(as.character(VarFreq))) %>%
-		select(Chrom, Position, End, Ref, VarAllele, VarFreq) %>%
-		arrange(Position, -VarFreq) %>%
-  		filter(duplicated(Position) == FALSE) %>% # If position is duplicated, only keep the variant with the higher allele frequency
-		rename(
-			Start = Position, 
-			Var = VarAllele) %>%
-		select(Chrom, Start, End, Ref, Var)
-
-	# COMBINED
-	df_main_combined <- rbind(df_ins, df_del) %>%
-		arrange(Chrom, Start)
-
-	write.table(df_main_combined, ANNOVAR_indel_input, sep="\t",  col.names=FALSE, row.names=FALSE, quote = FALSE)
+	write.table(df_main, ANNOVAR_indel_input, sep="\t", col.names=FALSE, row.names=FALSE, quote = FALSE)
 }
-
-# lapply(as.list(list.files(DIR_varscan_indel, full.names = TRUE, pattern = "\\.vcf$")), make_anno_input)
 
 make_anno_input(PATH_VarScan_indel = args$PATH_VarScan_indel,
 	ANNOVAR_indel_input = args$ANNOVAR_indel_input)
