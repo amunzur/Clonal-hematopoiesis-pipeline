10f02ba43d5113ab8bd35dc7fd97f461afab6308
amunzur <admunzur.21@gmail.com>
2022-05-12 10:47:13 -0700

    Removed unnecessary line

--- workflow/scripts/analysis/make_anno_input_indel.R
+++ workflow/scripts/analysis/make_anno_input_indel.R
@@ -12,30 +12,56 @@ parser$add_argument('--ANNOVAR_indel_input', metavar='FILE', type='character', h
 args <- parser$parse_args()
 
 # DIR_varscan_indel <- "/groups/wyattgrp/users/amunzur/chip_project/VarScan2_results/WBC_only/new_chip_panel_CP/indel"
-# PATH_VarScan_indel <- "/groups/wyattgrp/users/amunzur/chip_project/VarScan2_results/WBC_only/new_chip_panel_CP/indel/GUBB-17-267-gDNA-Baseline-IDT-2017Oct04_S16.vcf"
+# PATH_VarScan_indel <- "/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/indel/batch5/GUBB-19-407_cfDNA_Baseline_IDT_2019Jun05.vcf"
 
 # works on only one path. parallelize using lapply.
 make_anno_input <- function(PATH_VarScan_indel, ANNOVAR_indel_input) {
 
-	df <- as.data.frame(read.delim(PATH_VarScan_indel)) %>%
-			mutate(Position = as.numeric(Position), 
-					End = Position, 
-					VarAllele = as.character(VarAllele), 
-					Ref = as.character(Ref)) %>%
-			select(Chrom, Position, End, Ref, VarAllele)
+	df_main <- as.data.frame(read.delim(PATH_VarScan_indel)) %>%
+			mutate(
+				Var_type = case_when(
+					grepl("-", VarAllele) ~ "DEL", 
+					grepl("+", VarAllele) ~ "INS",
+					TRUE ~ "FUCK"), 
+				VarAllele = gsub("\\+|-", "", VarAllele))
 
-	names(df) <- c("Chrom", "Start", "End", "Ref", "Var")
+	# DELETIONS
+	df_del <- df_main %>%
+		filter(Var_type == "DEL") %>%
+		mutate(End = Position) %>%
+		select(Chrom, Position, End, VarAllele, Var_type, VarFreq) %>%
+		mutate(
+			Position = Position + 1, 
+			End = as.numeric(Position) + nchar(VarAllele) - 1, 
+			Var = "-", 
+			VarFreq = readr::parse_number(as.character(VarFreq))) %>%
+		arrange(Position, -VarFreq) %>%
+  		filter(duplicated(Position) == FALSE) %>% # If position is duplicated, only keep the variant with the higher allele frequency
+		rename(
+			Start = Position,
+			Ref = VarAllele) %>%
+		select(Chrom, Start, End, Ref, Var)
 
-	# deletions 
-	idx <- grep("\\-", df$Var)
-	df$Var[idx] <- "-"
-	df$Var <- gsub("\\+", "", df$Var)
+	# INSERTIONS
+	df_ins <- df_main %>%
+		filter(Var_type == "INS") %>%
+		mutate(
+			End = Position, 
+			Ref = "-", 
+			VarFreq = readr::parse_number(as.character(VarFreq))) %>%
+		select(Chrom, Position, End, Ref, VarAllele, VarFreq) %>%
+		arrange(Position, -VarFreq) %>%
+  		filter(duplicated(Position) == FALSE) %>% # If position is duplicated, only keep the variant with the higher allele frequency
+		rename(
+			Start = Position, 
+			Var = VarAllele) %>%
+		select(Chrom, Start, End, Ref, Var)
 
-	# insertions
-	idx <- grep("\\+", df$Var)
-	df$Ref[idx] <- "-"
+	# COMBINED
+	df_main_combined <- rbind(df_ins, df_del) %>%
+		arrange(Chrom, Start)
 
-	write.table(df, ANNOVAR_indel_input, sep="\t",  col.names=FALSE, row.names=FALSE, quote = FALSE)
+	write.table(df_main_combined, ANNOVAR_indel_input, sep="\t",  col.names=FALSE, row.names=FALSE, quote = FALSE)
 }
 
 # lapply(as.list(list.files(DIR_varscan_indel, full.names = TRUE, pattern = "\\.vcf$")), make_anno_input)
