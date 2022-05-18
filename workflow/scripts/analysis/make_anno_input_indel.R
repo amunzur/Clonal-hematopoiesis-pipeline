9a67d5e140914132b4eb043b0f4bd0d2b6422502
amunzur <admunzur.21@gmail.com>
2022-05-18 11:47:34 -0700

    Now considers the cases where two indels happen at the same position

--- workflow/scripts/analysis/make_anno_input_indel.R
+++ workflow/scripts/analysis/make_anno_input_indel.R
@@ -12,7 +12,7 @@ parser$add_argument('--ANNOVAR_indel_input', metavar='FILE', type='character', h
 args <- parser$parse_args()
 
 # DIR_varscan_indel <- "/groups/wyattgrp/users/amunzur/chip_project/VarScan2_results/WBC_only/new_chip_panel_CP/indel"
-# PATH_VarScan_indel <- "/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/indel/batch5/GUBB-19-407_cfDNA_Baseline_IDT_2019Jun05.vcf"
+# PATH_VarScan_indel <- "/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/indel/batch5/GUBB-19-476_cfDNA_Baseline_IDT_2019Oct25.vcf"
 
 # works on only one path. parallelize using lapply.
 make_anno_input <- function(PATH_VarScan_indel, ANNOVAR_indel_input) {
@@ -23,15 +23,16 @@ make_anno_input <- function(PATH_VarScan_indel, ANNOVAR_indel_input) {
 			Ref = Cons, 
 			Start = Position) %>%
 		mutate(
-			Var_type = case_when(
+			variant_type = case_when(
 				grepl("-", Ref) ~ "DEL", 
 				grepl("+", Ref) ~ "INS",
 				TRUE ~ "FUCK"), 
-			Start = as.numeric(Start) + 1,
-			Ref = gsub("[[:punct:]]", "", Ref), 
+			Start = ifelse(variant_type == "DEL", as.numeric(Start) + 1, as.numeric(Start)), # if DELETION, add 1 to the position. INS positions stay the way they are.
+			Ref = basename(as.character(Ref)), # to remove everything before the slash
+			Ref = gsub("[[:punct:]]", "", Ref), # consider everything but the punctuation, this one removes + and - signs from the ALT allele
 			Alt = Ref,
-			Alt = ifelse(Var_type == "DEL", "-", Alt), 
-			Ref = ifelse(Var_type == "INS", "-", Ref), 
+			Alt = ifelse(variant_type == "DEL", "-", Alt), 
+			Ref = ifelse(variant_type == "INS", "-", Ref), 
 			End = Start + nchar(Ref) - 1) %>%
 		select(Chrom, Start, End, Ref, Alt)			
 
