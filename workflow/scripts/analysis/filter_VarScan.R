d98b4225d08b8b781f55f5dcf93aee52285a6a6e
amunzur <admunzur.21@gmail.com>
2022-09-08 13:26:18 -0700

    removed cohort name from one of the inputs

--- workflow/scripts/analysis/filter_VarScan.R
+++ workflow/scripts/analysis/filter_VarScan.R
@@ -8,7 +8,6 @@ library(epitools)
 library(janitor)
 library(pkgcond)
 
-cohort_name <- args[[1]]
 variant_caller <- "varscan"
 
 THRESHOLD_ExAC_ALL <- 0.005
@@ -16,10 +15,10 @@ VALUE_Func_refGene <- "intronic"
 THRESHOLD_VarFreq <- 0.40
 THRESHOLD_Reads2 <- 5
 THRESHOLD_VAF_bg_ratio <- 10
-DIR_varscan_snv <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/snv", cohort_name)
-DIR_varscan_indel <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/indel", cohort_name)
-DIR_annovar_snv <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/data/annovar_outputs/snv", cohort_name)
-DIR_annovar_indel <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/data/annovar_outputs/indel", cohort_name)
+DIR_varscan_snv <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/snv")
+DIR_varscan_indel <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/indel")
+DIR_annovar_snv <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/data/annovar_outputs/snv")
+DIR_annovar_indel <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/data/annovar_outputs/indel")
 PATH_bg <- "/groups/wyattgrp/users/amunzur/pipeline/resources/bg_error_rate/bg_error.tsv"
 
 PATH_bets_somatic <- "/groups/wyattgrp/users/amunzur/pipeline/resources/betastasis/CLEANED_mutations_kidney_cancer_somatic.tsv"
@@ -27,14 +26,14 @@ PATH_bets_germline <- "/groups/wyattgrp/users/amunzur/pipeline/resources/betasta
 PATH_panel_genes <- "/groups/wyattgrp/users/amunzur/pipeline/resources/panel/kidney/genes.tsv"
 
 PATH_bed  <- "/groups/wyattgrp/users/amunzur/pipeline/resources/panel/1000012543_CHIP_Design_selection_results_Version2/capture_targets.bed"
-DIR_depth_metrics <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/metrics/depth", cohort_name)
-PATH_collective_depth_metrics <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/metrics/averaged_depth/PICARD_markdup", cohort_name, "averaged_depths.txt")
+DIR_depth_metrics <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/metrics/depth")
+PATH_collective_depth_metrics <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/metrics/averaged_depth/PICARD_markdup/averaged_depths.txt")
 DIR_tnvstats <- "/groups/wyattgrp/users/amunzur/pipeline/results/metrics/tnvstats/kidney_samples"
 DIR_temp <- "/groups/wyattgrp/users/amunzur/pipeline/results/temp"
 PATH_filter_tnvstats_script <- "/groups/wyattgrp/users/amunzur/pipeline/workflow/scripts/analysis/filter_tnvstats.sh"
 
 PATH_validated_variants <- "/groups/wyattgrp/users/amunzur/pipeline/resources/validated_variants/chip_muts_locations.tsv"
-PATH_SAVE_chip_variants <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/finalized", cohort_name, "chip_variants.csv")
+PATH_SAVE_chip_variants <- file.path("/groups/wyattgrp/users/amunzur/pipeline/results/variant_calling/VarScan2/finalized/chip_variants.csv")
 DIR_finland_bams <- "/groups/wyattgrp/data/bam/kidney"
 
 PATH_utilities_file_varscan <- "/groups/wyattgrp/users/amunzur/pipeline/workflow/scripts/analysis/UTILITIES_filter_varscan.R" # has the functions we use here 
